642d98d08fa6d7bc1ff99a74e68700733f98cfd5
Shailesh Yadav <sy05dec@gmail.com>
2020-02-16 21:26:34 +1300

    [Vulkan Tutorial] [1] Drawing a triangle [1] Setup [1.2] Instance

--- Triangle.h
+++ Triangle.h
@@ -6,30 +6,15 @@ const int WIDTH = 2048;
 const int HEIGHT = 1152;
 
 class HelloTriangleApplication {
-public:
-    void run() {
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
+    public:
+        void run();
+    private:
+        GLFWwindow* window;
+        VkInstance instance;
 
-private:
-GLFWwindow* window;
-    void initVulkan() {
-        glfwInit();
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-    }
-
-    void mainLoop() {
-        while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
-        }
-    }
-
-    void cleanup() {
-        glfwDestroyWindow(window);
-        glfwTerminate();
-    }
+        void initWindow();
+        void initVulkan();
+        void mainLoop();
+        void createInstance();
+        void cleanup();
 };
